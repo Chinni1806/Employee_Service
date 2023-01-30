3d16ef5f0a3098da387c50cf29f579251728b5c2
Chinni1806 <cherryaduri2000@gmail.com>
2023-01-30 14:53:09 +0530

    commit

--- src/main/java/com/accenture/Employee_Service/controller/EmpController.java
+++ src/main/java/com/accenture/Employee_Service/controller/EmpController.java
@@ -1,4 +1,5 @@
 package com.accenture.Employee_Service.controller;
+import com.accenture.Employee_Service.KafkaProducer.KafkaMessageProducer;
 import com.accenture.Employee_Service.Service.EmpService;
 import com.accenture.Employee_Service.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,18 @@ import java.util.Optional;
 @RequestMapping("/api/employee")
 public class EmpController {
     @Autowired
+    KafkaMessageProducer kafkaMessageProducer;
+    @Autowired
     EmpService empService;
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public void setEmployee(@RequestBody Employee employee){
         this.empService.setEmployee(employee);
+    }
+    @PostMapping("/message")
+    public String postMessage(@RequestBody String message){
+        kafkaMessageProducer.sendMessage(message);
+        return "Message Published";
     }
     @GetMapping("/{empId}")
     @ResponseStatus(HttpStatus.OK)
